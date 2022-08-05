3828472a6bb3c606a0af65e05434db926ed0c193
hyc <heyunchi1224@163.com>
2022-08-05 17:39:59 +0800

    案例

---
+++ dataframe.js
@@ -0,0 +1,19 @@
+import { DataFrame } from "dataframe-js";
+// From a dictionnary (Hash)
+// const df = new DataFrame({
+//     column1: [3, 6, 8], // <------ A column
+//     column2: [3, 4, 5, 6],
+//     quality: [4, 6, 1, 6]
+// }, ['column1', 'column2', 'quality']);
+// df.sortBy('quality')
+
+const df = new DataFrame([
+    [1, 6, 9, 10, 12], // <------- A row
+    [1, 2],
+    [6, 6, 9, 8, 9, 12],
+], ['c1', 'c2', 'c3', 'c4', 'c5', 'c6']);
+
+df.show();
+const data = df.toCollection()
+    // const data = df.toDict()
+console.log(data);
