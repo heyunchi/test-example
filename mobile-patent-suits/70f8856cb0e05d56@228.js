66bc0dac18dcb3bb80bf3bbd9a37c094561448c0
hyc <heyunchi1224@163.com>
2022-10-27 20:30:01 +0800

    test

--- mobile-patent-suits/70f8856cb0e05d56@228.js
+++ mobile-patent-suits/70f8856cb0e05d56@228.js
@@ -1,54 +1,59 @@
 // https://observablehq.com/@d3/mobile-patent-suits@228
 import define1 from "./a33468b95d0b15b0@808.js";
 
-function _1(md){return(
-md`# Mobile Patent Suits
+function _1(md) {
+    return (
+        md `# Mobile Patent Suits
 
 A view of patent-related lawsuits in the mobile communications industry, circa 2011. Data: [Thomson Reuters](http://blog.thomsonreuters.com/index.php/mobile-patent-suits-graphic-of-the-day/)`
-)}
+    )
+}
 
-function _2(Swatches,color){return(
-Swatches(color)
-)}
+function _2(Swatches, color) {
+    return (
+        Swatches(color)
+    )
+}
 
-function _chart(data,d3,width,height,types,color,location,drag,linkArc,invalidation)
-{
-  const links = data.links.map(d => Object.create(d));
-  const nodes = data.nodes.map(d => Object.create(d));
+function _chart(data, d3, width, height, types, color, location, drag, linkArc, invalidation) {
+    const links = data.links.map(d => Object.create(d));
+    const nodes = data.nodes.map(d => Object.create(d));
 
-  const simulation = d3.forceSimulation(nodes)
-      .force("link", d3.forceLink(links).id(d => d.id))
-      .force("charge", d3.forceManyBody().strength(-400))
-      .force("x", d3.forceX())
-      .force("y", d3.forceY());
+    const simulation = d3.forceSimulation(nodes)
+        .force("link", d3.forceLink(links).id(d => {
+            return d.id
+        }))
+        .force("charge", d3.forceManyBody().strength(-400))
+        .force("x", d3.forceX())
+        .force("y", d3.forceY());
 
-  const svg = d3.create("svg")
-      .attr("viewBox", [-width / 2, -height / 2, width, height])
-      .style("font", "12px sans-serif");
+    const svg = d3.create("svg")
+        .attr("viewBox", [-width / 2, -height / 2, width, height])
+        .style("font", "12px sans-serif");
 
-  // Per-type markers, as they don't inherit styles.
-  svg.append("defs").selectAll("marker")
-    .data(types)
-    .join("marker")
-      .attr("id", d => `arrow-${d}`)
-      .attr("viewBox", "0 -5 10 10")
-      .attr("refX", 15)
-      .attr("refY", -0.5)
-      .attr("markerWidth", 6)
-      .attr("markerHeight", 6)
-      .attr("orient", "auto")
-    .append("path")
-      .attr("fill", color)
-      .attr("d", "M0,-5L10,0L0,5");
+    // Per-type markers, as they don't inherit styles.
+    svg.append("defs").selectAll("marker")
+        .data(types)
+        .join("marker")
+        .attr("id", d => `arrow-${d}`)
+        .attr("viewBox", "0 -5 10 10")
+        .attr("refX", 15)
+        .attr("refY", -0.5)
+        .attr("markerWidth", 6)
+        .attr("markerHeight", 6)
+        .attr("orient", "auto")
+        .append("path")
+        .attr("fill", color)
+        .attr("d", "M0,-5L10,0L0,5");
 
-  const link = svg.append("g")
-      .attr("fill", "none")
-      .attr("stroke-width", 1.5)
-    .selectAll("path")
-    .data(links)
-    .join("path")
-      .attr("stroke", d => color(d.type))
-      .attr("marker-end", d => `url(${new URL(`#arrow-${d.type}`, location)})`);
+    const link = svg.append("g")
+        .attr("fill", "none")
+        .attr("stroke-width", 1.5)
+        .selectAll("path")
+        .data(links)
+        .join("path")
+        .attr("stroke", d => color(d.type))
+        .attr("marker-end", d => `url(${new URL(`#arrow-${d.type}`, location)})`);
 
   const node = svg.append("g")
       .attr("fill", "currentColor")
